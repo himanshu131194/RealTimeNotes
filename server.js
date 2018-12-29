37fd57ef598671f6080e3836402edd6891010b9f
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2018-12-29 11:49:42 +0530

    go to heroku

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 
 const mongoose = require('mongoose');
       mongoose.connect("mongodb://siliconpay:siliconpay123@ds127888.mlab.com:27888/siliconpay",  { useNewUrlParser: true });
 const Users = require('./models/users')(mongoose);
 
 
-app.use(express.static(__dirname+'/public'));
+//app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 
-(async ()=>{
-  const users = new Users({
-        text: "dssdsdsds"
-  })
-  await users.save();
-})();
+io.on('connection', function(socket){
+     console.log('a user connected');
+})
 
 
+// (async ()=>{
+//   const users = new Users({
+//         text: "dssdsdsds"
+//   })
+//   await users.save();
+// })();
 
-app.listen(4001, ()=>{
-    console.log(`server is running on port 4001`);;
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`server is running on port 3000`);;
 });
